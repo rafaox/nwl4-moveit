6becbf660185ed990ed29d282e2f27440bdbb4d3
Rafael Cardoso <rflcardoso90@gmail.com>
2021-02-28 22:45:48 -0300

    First commit to next level week 4 - Mission complete

---
+++ src/components/CompletedChallanges.tsx
@@ -0,0 +1,18 @@
+
+
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/CompletedChallanges.module.css'
+
+export function CompletedChallanges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.completedChallangesContainer}>
+      <span>Desafios completos</span>
+      <span>{challengesCompleted}</span>
+    </div>
+  );
+}
